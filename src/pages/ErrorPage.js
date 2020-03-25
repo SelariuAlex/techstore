b954da31ef695eafe09846c87ecc0535dead3220
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-03-25 12:26:33 +0200

    add error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-  return <h1>hello form error page</h1>;
+  return (
+    <div className="error-page section">
+      <div className="error-container">
+        <h1>Page not found 404</h1>
+        <Link to="/" className="btn btn-primary">
+          Go to Home page
+        </Link>
+      </div>
+    </div>
+  );
 }
