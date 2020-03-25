953e5cee73d588e1050822cf8a8a8208b9782386
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-03-25 12:29:54 +0200

    add about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolorem
+        rem eum exercitationem placeat molestias debitis pariatur, rerum, a
+        impedit cupiditate quia quibusdam accusantium sapiente? Omnis earum hic,
+        non adipisci esse accusamus! Earum vitae delectus doloribus voluptatibus
+        veritatis veniam tempora inventore, voluptate, quasi beatae et corporis
+        voluptas voluptatem? Dolorum, cupiditate!
+      </p>
+    </section>
+  );
 }
