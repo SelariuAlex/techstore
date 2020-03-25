84c458c6a40f10229c3980fdb0067817c5e59192
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-03-26 00:07:05 +0200

    hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Hero() {
-  return <h1>hello from hero</h1>;
+export default function Hero({ children }) {
+  return (
+    <div className="hero">
+      <div className="banner">
+        <h1>Code, test , deploy</h1>
+        <p>Use your code for good, we do!</p>
+        {children}
+      </div>
+    </div>
+  );
 }
