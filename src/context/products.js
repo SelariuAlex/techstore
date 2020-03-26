00aad5a55d1d08d6d0f0f00181a472a9a3784ea8
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-03-27 00:36:11 +0200

    get products with axios

--- src/context/products.js
+++ src/context/products.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, createContext, useEffect } from "react";
+import axios from "axios";
+import url from "../utils/URL";
 
-export const ProductContext = React.createContext();
+export const ProductContext = createContext();
 
 export default function ProductProvider({ children }) {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [featured, setFeadtured] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${url}/products`).then(response => {
+      setProducts(response.data);
+    });
+    return () => {};
+  }, []);
 
   return (
     <ProductContext.Provider value={{ loading, products, featured }}>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,5 +4,7 @@ import { ProductContext } from "../context/products";
 export default function Products() {
   const { loading, products, featured } = useContext(ProductContext);
 
+  console.log(products);
+
   return <h1>hello from products page</h1>;
 }
