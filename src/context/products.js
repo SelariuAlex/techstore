1544466d79eb2b1f636d0f74b57e5613e12b49d5
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-03-26 11:49:38 +0200

    set up products context

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,15 @@
-// products context
+import React, { useState } from "react";
+
+export const ProductContext = React.createContext();
+
+export default function ProductProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [featured, setFeadtured] = useState([]);
+
+  return (
+    <ProductContext.Provider value={{ loading, products, featured }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
